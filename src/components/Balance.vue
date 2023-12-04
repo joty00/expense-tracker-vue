c7267301e256af67a41d664e31dffd1494e13652
Jorge Castilla <jorgitocastilla27@gmail.com>
2023-12-04 18:39:38 -0500

    first commit

---
+++ src/components/Balance.vue
@@ -0,0 +1,20 @@
+<template>
+    <main>
+        <h4>Your Balance</h4>
+        <h1 id="balance">${{ total }}</h1>
+    </main>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+    const props = defineProps ({
+        total: {
+            type: Number,
+            required: true,
+        }
+    }) 
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,34 @@
+<template>
+   <main>
+    <h3>History</h3>
+      <ul id="list" class="list">
+        <li v-for="transaction in transactions" 
+        :key="transaction.id" 
+        :class="transaction.amount < 0 ? 'minus' : 'plus'">
+          {{ transaction.text }} <span>${{ transaction.amount }}</span>
+          <button class="delete-btn" @click="deleteTransaction(transaction.id)">x</button>
+        </li>
+      </ul>
+   </main>
+</template>
+
+<script setup>
+  import { defineProps } from 'vue';
+
+  const emit = defineEmits(['transactionDeleted'])
+
+  const props = defineProps({
+    transactions: {
+      type: Array,
+      required: true,
+    }
+  })
+
+  const deleteTransaction = (id) => {
+    emit('transactionDeleted', id)
+  }
+</script>
+
+<style>
+
+</style>
